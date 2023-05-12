42cde9ed6e42f62289cd4a7d22e1e8e25ae40966
Gowtham S <gowsrini2004@gmail.com>
2023-05-13 01:34:26 +0530

    writing to firebase done

--- py1.py
+++ py1.py
@@ -1,1 +1,22 @@
-print("Hekkk")
+import firebase_admin as f
+from firebase_admin import credentials,firestore
+cred = credentials.Certificate("credentials.json")
+f.initialize_app(cred)
+db = firestore.client()
+n = int(input("Enter No Of Entries: "))
+data = []
+for i in range(n):
+    name = input("Enter Name: ")
+    age = int(input("Enter Age: "))
+    net_worth = int(input("Enter Net Worth: "))
+    s = "var_"+str(i)
+    s = {
+    'Name':name,
+    'Age':age,
+    'Networth':net_worth        
+    }
+    data.append(s)
+for rec in data:
+    doc_ref = db.collection(u'Users').document(rec['Name'])
+    doc_ref.set(rec)
+print("Done")
